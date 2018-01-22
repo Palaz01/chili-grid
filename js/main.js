edc96b5d8b24cf29560ad1f47279a95ed9c0bf3d
palaz <barnabas.palatinus@gmail.com>
2018-01-22 19:46:07 +0100

    species page grid & some javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function(){
+
+   // Navigation
+
    $('.menu-button').on('click', function(){
       $('#main-nav').toggleClass('open');
    });
@@ -10,4 +13,49 @@ $(function(){
    $(window).on('scroll', function(){
       $('#main-nav').removeClass('open');
    });
+
+   // Species page
+
+   $('#light').change(function(evt){
+      updateView('light', evt.target.checked);
+   });
+
+   $('#medium').change(function(evt){
+      updateView('medium', evt.target.checked);
+   });
+
+   $('#killer').change(function(evt){
+      updateView('killer', evt.target.checked);
+   }); 
+
+   $('#chili-species').find('.chili-item:visible:first').addClass('chili-item-highlighted');
+
 });
+
+function updateView(categoryName, bVisible) {
+   var dataSelectorVal = "";
+   switch (categoryName) {
+      case "light":
+         dataSelectorVal = "img[data-type='light']";
+         break;
+      case "medium":
+         dataSelectorVal = "img[data-type='medium']";
+         break;
+      case "killer":
+         dataSelectorVal = "img[data-type='killer']";
+         break;
+   }
+
+   $('.chili-item').has(dataSelectorVal).css('display', bVisible ? "" : "none");
+}
+
+   
+
+
+   
+
+
+
+  
+
+   
